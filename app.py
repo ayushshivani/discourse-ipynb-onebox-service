953fb7b90a16cf74511f3fb101b088c3a349f1bd
Ayush Shivani <ashivani@aicrowd.com>
2020-05-13 20:36:50 +0530

    added support for colab notebook

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import requests
 from flask import Flask
 from flask import request
 import json
+import re
 
 app = Flask(__name__)
 BASE_URL = 'https://api.github.com'
@@ -67,6 +68,26 @@ def create_gist(content,challenge_name):
     colab_url = COLAB_URL + USER_NAME +"/" + gist_id
     return colab_url
 
+def authentication(url,provided_hash):
+    # Some authenticatioon
+    secret = os.getenv("ONEBOX_IPYNB_RENDERER_SECRET", "AIcrowd-magic")
+    generated_hash = hashlib.md5((secret+url).encode()).hexdigest()
+    if provided_hash != generated_hash:
+        return 'failed'
+       return generated_hash
+
+
+def convert_tohtml(download_path):
+    try:
+        import nbconvert
+        exportor = nbconvert.exporters.HTMLExporter(template_file='basic')
+        generated_html = exportor.from_filename(download_path)[0]
+    except Exception as e:
+        raise e
+        return '<pre>nbconvert parsing error</pre>'
+
+    return generated_html
+
 
 
 @app.route('/render/ipynb')
@@ -76,13 +97,9 @@ def ipynb_handler():
     provided_hash = request.args.get('hash')
     if not url:
         return '<pre>Unable to render, URL not provided</pre>'
-
-    # # Some authenticatioon
-    secret = os.getenv("ONEBOX_IPYNB_RENDERER_SECRET", "AIcrowd-magic")
-    generated_hash = hashlib.md5((secret+url).encode()).hexdigest()
-
-    if provided_hash != generated_hash:
-        return '<pre>Authentication failed</pre>'
+    generated_hash = authentication(url,provided_hash)
+    if generated_hash == 'failed':
+        return '<pre>Authentication Failed</pre>'
 
     # Downloading the ipynb and 5 minutes caching
     try:
@@ -99,14 +116,7 @@ def ipynb_handler():
         raise e
         return '<pre>Error fetching ipynb file</pre>'
 
-    # nbconvert
-    try:
-        import nbconvert
-        exportor = nbconvert.exporters.HTMLExporter(template_file='basic')
-        generated_html = exportor.from_filename(download_path)[0]
-    except Exception as e:
-        raise e
-        return '<pre>nbconvert parsing error</pre>'
+    generated_html = convert_tohtml(download_path)
 
     custom_start = open('start.html', 'r').read()
     raw_url = url.replace("blob","raw")
@@ -122,3 +132,56 @@ def ipynb_handler():
 
     return custom_start + generated_html + custom_end
 
+
+@app.route('/render/colab')
+def colab_handler():
+    # Input the parameters
+    url = request.args.get('url')
+    colab_id = re.findall("[-\w]{25,}", url)[0]
+    provided_hash = request.args.get('hash')
+    if not url:
+        return '<pre>Unable to render, URL not provided</pre>'
+
+    generated_hash = authentication(url,provided_hash)
+    if generated_hash == 'failed':
+        return '<pre>Authentication Failed</pre>'
+
+
+    drive_url = "https://docs.google.com/uc?export=download"
+
+    # Downloading the ipynb and 5 minutes caching
+    try:
+        download_path = 'download/' + generated_hash 
+        if os.path.isfile(download_path) and time.time() - os.stat(download_path).st_mtime < 300:
+            content = open(download_path, 'r').read()
+        else:
+            response = requests.get(drive_url, params = { 'id' : colab_id }, stream = True)
+            CHUNK_SIZE = 32768
+            if os.path.isfile(download_path):
+                os.remove(download_path)
+            
+            with open(download_path, "wb") as f:
+                for chunk in response.iter_content(CHUNK_SIZE):
+                    if chunk: # filter out keep-alive new chunks
+                        f.write(chunk)
+            content = open(download_path, 'r').read()
+            
+
+    except Exception as e:
+        raise e
+        return '<pre>Error fetching ipynb file</pre>'
+
+    generated_html = convert_tohtml(download_path)
+
+    custom_start = open('start.html', 'r').read()
+    download_url = drive_url+"&id=" +colab_id
+
+    colab_url = create_gist(content,generated_hash)
+
+    download_button = '<a href="' + download_url + '"class=btn btn-primary"><i class="fa fa-download"> Download</i></a>'
+    colab_button = '<a href="' + url + '" target="_blank" class="btn btn-primary"><i class="fas fa-infinity"> Execute In Colab</i></a>'
+    custom_start += '<div style="text-align: center;">' + download_button + colab_button + '</div>'
+
+    custom_end = open('end.html', 'r').read()
+
+    return custom_start + generated_html + custom_end
